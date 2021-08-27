bcd22c7bcf12a61d48e488bf52715abbd0cef559
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    sm6375-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- common.mk
+++ common.mk
@@ -285,6 +285,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
     ro.carrier
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # RFS MSM MPSS symlinks
 PRODUCT_PACKAGES += \
     rfs_msm_mpss_readonly_vendor_fsg_symlink
